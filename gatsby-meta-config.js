1d4a951563a811ca991011cf550b0c75ad0aa7fb
Seokyung Jung <tjrud1438@naver.com>
2021-05-20 20:44:22 +0900

    feat: 설정 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: `My Third Tech Blog`,
+  title: `My Tech Blog`,
   description: `Blog posted about ...`,
   author: `Seokyung`,
   introduction: `매일 개발 일지를 씁니다. 그리고..`,
-  siteUrl: `https://jolly-wiles-40126b.netlify.app`, // Your blog site url
+  siteUrl: `https://amiacoolgirl-dev.netlify.app/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `https://github.com/imacoolgirlyo`, // Your GitHub account
